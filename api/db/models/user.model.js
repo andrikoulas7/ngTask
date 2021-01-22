8e55bdbf25c66f84794a38fc95dada4c43b8fa70
andrikoulas7 <panagandrea@gmail.com>
2021-01-22 18:47:03 +0200

    Add - User login methods

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const jwtSecret = "00111591611510489533asvsdjfaoslvkfgs9709481138";
+const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -36,6 +39,55 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAccessAuthToken = function () {
     const user = this;
     return new Promise((resolve, reject) => {
-
+        jwt.sign({ _id: user._id.toHexString() }, jwtSecret, { espiresIn: "15m" }, (err, token) => {
+            if (!err) {
+                resolve(token);
+            } else {
+                reject();
+            }
+        })
     })
+}
+
+UserSchema.methods.generateRefreshAuthToken = function () {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(64, (err, buf) => {
+            if (!err) {
+                let token = buf.toString('hex');
+
+                return resolve(token);
+            }
+        })
+    })
+}
+
+UserSchema.methods.createSession = function () {
+    let user = this;
+    return user.generateRefreshAuthToken().then((refreshToken) => {
+        return saveSessionToDatabase(user, refreshToken);
+    }).then((refreshToken) => {
+        return refreshToken;
+    }).catch((e) => {
+        return Promise.reject('Failed to save session to database. \n' + e);
+    })
+}
+
+let saveSessionToDatabase = (user, refreshToken) => {
+    return new Promise((resolve, reject) => {
+        let expiresAt = generateRefreshTokenExpiryTime();
+
+        user.sessions.push({ 'token': refreshToken, expiresAt });
+
+        user.save().then(() => {
+            return resolve(refreshToken);
+        }).catch((e) => {
+            reject(e);
+        })
+    })
+}
+
+let generateRefreshTokenExpiryTime = () => {
+    let daysUntilExpire = "10";
+    let secondsUntilExpire = ((daysUntilExpire * 24) * 60) * 60;
+    return ((Date.now() / 1000) + secondsUntilExpire);
 }
