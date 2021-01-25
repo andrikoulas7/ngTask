4ea0ec7a52196bd787bf161bf9fec33a2f0d3d82
andrikoulas7 <panagandrea@gmail.com>
2021-01-25 18:43:40 +0200

    Add - Delete task from list

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -17,9 +17,12 @@ export class TaskService {
     return this.webReqService.patch(`lists/${id}`, { title });
   }
 
-
   createTask(title: string, listId: string) {
     return this.webReqService.post(`lists/${listId}/tasks`, { title });
+  }
+
+  deleteTask(listId: string, taskId: string) {
+    return this.webReqService.delete(`lists/${listId}/tasks/${taskId}`);
   }
 
   getLists() {
